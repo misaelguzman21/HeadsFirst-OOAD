54b11cf05df4098000ea4d4f12d85bc015837d37
Misael Guzmán Gutiérrez <misaelguzmangutierrez@Mac-mini-de-Misael.local>
2025-05-15 14:24:33 -0600

    exercise 3 from chapter 1

---
+++ Chapter-1/ricksGuitar-encapsulation/Guitar.java
@@ -0,0 +1,39 @@
+public class Guitar {
+
+  private String serialNumber;
+  private double price;
+  GuitarSpec spec;
+;
+
+  /**
+   * @param serialNumber
+   * @param price
+   * @param builder
+   * @param model
+   * @param type
+   * @param backWood
+   * @param topWood
+   */
+  public Guitar(String serialNumber, double price,
+                Builder builder, String model, Type type,
+                Wood backWood, Wood topWood) {
+    this.serialNumber = serialNumber;
+    this.price = price;
+    this.spec = new GuitarSpec(builder, model, type, backWood, topWood);
+  }
+
+  public String getSerialNumber() {
+    return serialNumber;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+  public void setPrice(float newPrice) {
+    this.price = newPrice;
+  }
+
+  public GuitarSpec getSpec(){
+    return spec;
+  }
+}
---
+++ Chapter-1/ricksGuitar-encapsulation/GuitarSpec.java
@@ -0,0 +1,36 @@
+public class GuitarSpec {
+
+    private String model;
+    private Builder builder;
+    private Type type;
+    private Wood backWood, topWood;
+
+    public GuitarSpec(Builder builder, String model, Type type, Wood backWood, Wood topWood){
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+    }
+
+    public Builder getBuilder(){
+        return this.builder;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public Type getType(){
+        return this.type;
+    }
+
+    public Wood getBackWood(){
+        return this.backWood;
+    }
+
+    public Wood getTopWood(){
+        return this.topWood;
+    }
+
+}
